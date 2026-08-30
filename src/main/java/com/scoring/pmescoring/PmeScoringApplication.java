49fc5e94622d5ad73f5ac9dc0728e8de8d41dac1
RiskScoreTcc <riskscoretcc@gmail.com>
2026-08-30 13:28:28 -0300

    chore: initial project structure and database configuration

---
+++ src/main/java/com/scoring/pmescoring/PmeScoringApplication.java
@@ -0,0 +1,13 @@
+package com.scoring.pmescoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmeScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmeScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoring/pmescoring/PmeScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoring.pmescoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmeScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
